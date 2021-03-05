b4f7467cd71f2fdc2af0bfae805c5d303d7f43a6
sota.morita <kemonoriginal@gmail.com>
2021-03-05 14:58:44 +0900

    SECRET_KEYを再削除

--- moneyproject/settings.py
+++ moneyproject/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bj4u2@_eazw!*w(0!gj!d3ywsij+ap@hn)%y+&mj-ga*sq(493'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
